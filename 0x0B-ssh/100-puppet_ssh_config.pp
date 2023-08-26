de8f1e5187c3d0575fc587e4c667dbc12a3fe21d
Thomas Collins <collinskthomas@gmail.com>
2023-08-26 13:54:19 +0000

    Add puppet file that makes changes to ssh config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# puppet config to make changes to an ssh config file
+
+file_line {'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '  IdentityFile ~/.ssh/school',
+}
+
+file_line {'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '  PasswordAuthentication no',
+}
