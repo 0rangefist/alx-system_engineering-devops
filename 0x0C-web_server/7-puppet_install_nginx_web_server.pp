435d314188c37769ac7eed67c0af006244d2c71c
Thomas Collins <collinskthomas@gmail.com>
2023-08-30 14:12:05 +0000

    Update puppet file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,30 @@
 # Puppet config for nginx server
 
-# Install nginx
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-# Allow firewall connetion on HTTP Port 80
-exec { 'permit_http':
-  command => 'ufw allow "Nginx HTTP"',
-  require => Package['nginx'],
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
 
-# Create index.html file with "Hello World!"
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-# Configure redirect 301
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => inline_template("
-<% if ! File.read('/etc/nginx/sites-available/default').include?('location /redirect_me {') %>
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-<% end %>
-  "),
-  require => Package['nginx'],
-}
+  content => '
+server {
+  listen 80;
+  server_name _;
 
-# Restart Nginx
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+  location /redirect_me {
+    return 301 "https://www.example.com/your-target-url";
+  }
+
+  location / {
+    return 200 "Hello World!";
+  }
+}
+',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
