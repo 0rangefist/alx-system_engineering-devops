353ab925b8a496c34776097a0d02cabed73b89bc
Thomas Collins <collinskthomas@gmail.com>
2023-09-04 23:47:40 +0000

    Update Puppet file to handle full new server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,51 +1,64 @@
-# Puppet config for nginx server
+class nginx_install {
+  package { 'nginx':
+    ensure => installed,
+  }
 
-# install nginx
-package { 'nginx':
-  ensure => installed,
-}
+  service { 'nginx':
+    ensure => 'running',
+    enable => true,
+    require => Package['nginx'],
+  }
 
-# start the service
-service { 'nginx':
-  ensure => running,
-  enable => true,
-}
+  exec { 'allow_firewall_http':
+    command => 'ufw allow Nginx HTTP',
+    unless  => 'ufw status | grep "Nginx HTTP"',
+    require => Service['nginx'],
+  }
 
-# create the index file with content "Hello World!"
-# and notify the nginx service to restart
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
+  file { '/var/www/html/index.html':
+    ensure  => 'file',
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
 
-# create the nginx default config with basic settings,
-# a 301 redirect and notify nginx service to restart
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => '
+  file { '/var/www/html/404.html':
+    ensure  => 'file',
+    content => "Ceci n'est pas une page",
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => 'file',
+    content => "
 server {
   listen 80;
-
   add_header X-Served-By $hostname;
-
   server_name _;
 
   root /var/www/html;
   index index.html index.nginx-debian.html;
 
   location / {
-    # First attempt to serve request as file, then
-    # as directory, then fall back to displaying a 404.
     try_files $uri $uri/ =404;
   }
 
   location /redirect_me {
-    return 301 "https://www.example.com/your-target-url";
+    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+  }
+
+  location /404.html {
+    root /var/www/html;
   }
 }
-',
-  require => [File['/var/www/html/index.html'], Package['nginx']],
-  notify  => Service['nginx'],
+",
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  # Restart nginx if config file is modified
+  exec { 'restart_nginx':
+    command     => 'service nginx restart',
+    refreshonly => true,
+    subscribe   => File['/etc/nginx/sites-available/default'],
+  }
 }
