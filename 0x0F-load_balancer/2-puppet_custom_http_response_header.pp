bff7929f27c4a203eef333e56f9a890919246aff
Thomas Collins <collinskthomas@gmail.com>
2023-09-04 17:57:52 +0000

    Update puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,12 +11,41 @@ service { 'nginx':
   enable => true,
 }
 
-# add custom header
-file_line { 'custom_header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
+# create the index file with content "Hello World!"
+# and notify the nginx service to restart
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => "Hello World!\n",
   require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# create the nginx default config with basic settings,
+# a 301 redirect and notify nginx service to restart
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+server {
+  listen 80;
+
+  add_header X-Served-By $hostname;
+
+  server_name _;
+
+  root /var/www/html;
+  index index.html index.nginx-debian.html;
+
+  location / {
+    # First attempt to serve request as file, then
+    # as directory, then fall back to displaying a 404.
+    try_files $uri $uri/ =404;
+  }
+
+  location /redirect_me {
+    return 301 "https://www.example.com/your-target-url";
+  }
+}
+',
+  require => [File['/var/www/html/index.html'], Package['nginx']],
   notify  => Service['nginx'],
 }
