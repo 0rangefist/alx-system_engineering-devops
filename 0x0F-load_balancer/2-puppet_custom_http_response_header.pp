e849cac989cae67e45713280c27487df8af717a8
Thomas Collins <collinskthomas@gmail.com>
2023-09-06 12:45:25 +0000

    Edit puppet file again

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,14 @@
 # Puppet config for nginx server
 
+# make sure package repos are up to date
+exec { 'apt_update':
+  command => '/usr/bin/apt-get update',
+}
+
 # install nginx
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['apt_update'],
 }
 
 # start the service
@@ -11,13 +17,27 @@ service { 'nginx':
   enable => true,
 }
 
-# add custom header
-file_line { 'custom_header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'server_name _;',
-  line    => 'add_header X-Served-By $hostname;',
+# add config file with custom header
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+server {
+  listen 80 default_server;
+
+  server_name _;
+ 
+  add_header X-Served-By $hostname;
+
+  root /var/www/html;
+  index index.html index.nginx-debian.html;
+
+  location / {
+    # First attempt to serve request as file, then
+    # as directory, then fall back to displaying a 404.
+    try_files $uri $uri/ =404;
+  }
+}
+',
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
-
