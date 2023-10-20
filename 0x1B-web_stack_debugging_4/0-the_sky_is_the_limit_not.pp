7c0f377fc256faecb736618125ab0aa2f1fb39e9
Thomas Collins <collinskthomas@gmail.com>
2023-10-20 11:56:41 +0000

    Update puppet file fo web-stack debugging 4 task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,10 @@
 # Puppet file to fix low ULIMIT in nginx server
 
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => "ULIMIT=\"-n 8192\"\n",
+exec { 'fix_ulimit':
+  command => "/bin/sed -i 's/15/8192/g' /etc/default/nginx",
 }
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/default/nginx'],
+exec { 'nginx_restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
